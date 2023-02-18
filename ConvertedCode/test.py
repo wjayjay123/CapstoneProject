31a924a01ac1e58362a7386428863848b243583e
wjayjay123 <77979635+wjayjay123@users.noreply.github.com>
2023-02-18 20:42:28 +0800

    Update test.py

--- ConvertedCode/test.py
+++ ConvertedCode/test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from tvi import tvi
 import math
+from quantizeNL_float import quantizeNL_float
 
 # a = [-3.95]
 
@@ -16,9 +17,13 @@ import math
 
 # print(b,c,d,e,f)
 
-a = [[10,20,30],[40,50,60],[70,80,90]]
+# a = [[10,20,30],[40,50,60],[70,80,90]]
+# b = np.array(a)
+# print(b)
+# b = pow(b,3)
+# print(b)
+# print(b.size)
+
+a = [[10,20,30],[40,50,60],[70,80,90]];
 b = np.array(a)
-print(b)
-b = pow(b,3)
-print(b)
-print(b.size)
+quantizeNL_float(b,55,b)
