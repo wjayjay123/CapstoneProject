00bf8371667cac33e2186de33d603eb27274afd4
wjayjay123 <77979635+wjayjay123@users.noreply.github.com>
2023-03-06 21:09:36 +0800

    Update quantizeNL_float.py

--- ConvertedCode/quantizeNL_float.py
+++ ConvertedCode/quantizeNL_float.py
@@ -2,7 +2,11 @@
 import numpy as np
 import math
 import statistics
+import sys
 from tvi import tvi
+
+#Declare epsilon value
+eps = sys.float_info[8]
 
 def quantizeNL_float(y, nclust, lum):
     lum0 = lum
@@ -10,7 +14,7 @@ def quantizeNL_float(y, nclust, lum):
     lum = np.sort(lum)
     lum = lum[0]
 
-    y = np.reshape(y, (1,y.size))
+    y = np.reshape(y, (1,np.size(y)))
     y = np.sort(y)
 
     edges = np.array([0, y.size])
@@ -27,54 +31,62 @@ def quantizeNL_float(y, nclust, lum):
         n = edges[idx+1] - k
         sn = s_data[(k+n)-1]
         if(k >= 1):
-            sn = sn - s_data[k]
+            sn = sn - s_data[k-1]
         ssn = ss_data[(k+n)-1]
         if(k >= 1):
-            ssn = ssn - ss_data[k]
+            ssn = ssn - ss_data[k-1]
         d = 2
         m = math.floor(n/d)
-        i = 0
-        while(i<1):
+
+        while(1):
             sm = s_data[(k+m)-1]
             if(k >= 1):
-                sm = sm - s_data[k]
+                sm = sm - s_data[k-1]
             ssm = ss_data[(k+m)-1]
             if(k >= 1):
-                ssm = ssm - ss_data[k]
+                ssm = ssm - ss_data[k-1]
             e1 = ssm - pow(sm,2)/m
             e2 = ssn - ssm - pow((sn - sm),2)/(n - m)
             d = 2 * d
-            # if(abs(e1-e2) < 0.001 or d >= n):
-            lum1 = statistics.median(lum[k:k+m])
-            lum2 = statistics.median(lum[k+m:k+n])
-            delta1 = pow(10,tvi(math.log10(lum1)))
-            delta2 = pow(10,tvi(math.log10(lum2)))
-            lum_loc = np.argmin(abs(delta1 / (delta1 + delta2) * (lum[(k+n-1)] - lum[k]) + lum[k] - lum[k:(k+n)]))
-            m = lum_loc + 1
-            sm = s_data[(k+m)-1]
-            if(k >= 1):
-                sm = sm - s_data[k]
-            ssm = ss_data[(k+m)-1]
-            if(k >= 1):
-                ssm = ssm - ss_data[k]
-            e1 = ssm - pow(sm,2)/m
-            e2 = ssn - ssm - pow((sn - sm),2) / (n - m)
+            if(abs(e1-e2) < 0.001 or d >= n):
+                lum1 = statistics.median(lum[k:k+m])
+                lum2 = statistics.median(lum[k+m:k+n])
+                delta1 = pow(10,tvi(math.log10(lum1)))
+                delta2 = pow(10,tvi(math.log10(lum2)))
+                lum_loc = np.argmin(abs(delta1 / (delta1 + delta2) * (lum[(k+n-1)] - lum[k]) + lum[k] - lum[k:(k+n)]))
+                m = lum_loc + 1
+                sm = s_data[(k+m)-1]
+                if(k >= 1):
+                    sm = sm - s_data[k-1]
+                ssm = ss_data[(k+m)-1]
+                if(k >= 1):
+                    ssm = ssm - ss_data[k-1]
+                e1 = ssm - pow(sm,2)/m
+                e2 = ssn - ssm - pow((sn - sm),2) / (n - m)
+                edges = np.insert(edges,idx+1,(k+m))
 
-            edges = np.insert(edges,idx+1,(k+m))
-            print(idx)
-            if(len(errors[0:idx-1]) == 0 and len(errors[idx+1:len(errors)-1]) == 0):
-                errors = np.array([e1,e2])
-            elif(len(errors[0:idx-1]) == 0):
-                errors = errors[idx+1:len(errors)-1]
-                print("iferror")
-                print(errors)
-            print("test")
-            print(edges)
-            print(errors)
-            # print(edges)
-            # tempError = errors[idx:len(errors)-1]
-            # errors = [errors[0:idx-1],e1,e2]
-            # # errors = np.append(errors,tempError)
-            # print(errors)
-
-            i+=1
+                if(len(errors[:idx]) == 0):
+                    errors = np.array([e1,e2])
+                elif(len(errors[idx+1:]) == 0):
+                    errors = errors[:idx]
+                    errors = np.insert(errors,idx,[e1,e2])
+                else:
+                    errors = np.insert(errors,idx,[e1,e2])
+                break
+            else:
+                if(e1 > e2):
+                    m = m - math.floor(n/d);
+                elif(e1 < e2):
+                    m = m + math.floor(n/d);
+    
+    mdata = np.zeros(nclust)
+    mdata[0] = np.min(lum0)
+    mdata[-1] = np.max(lum0)
+    for i in range(1, nclust-1):
+        if lum[edges[i]-1]==lum[edges[i+1]-1]:
+            ind = lum0[lum0==lum[edges[i]-1]]
+            mdata[i] = np.mean(ind)+eps*i
+        else:
+            ind = lum0[(lum0 > lum[edges[i]-1]) & (lum0 <= lum[edges[i+1]-1])]
+            mdata[i] = np.mean(ind)
+            print(mdata[i])
